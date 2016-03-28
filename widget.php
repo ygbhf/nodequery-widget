02d9b808cddd2dd23fb584570593bfd129171c06
Conna <connawiles@gmail.com>
2016-03-28 14:17:46 +0100

    Test

--- widget.php
+++ widget.php
@@ -1,3 +1,12 @@
 <?php
+$im = imagecreatetruecolor(120, 20);
+$text_color = imagecolorallocate($im, 233, 14, 91);
 
+imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
+
+header('Content-Type: image/jpeg');
+
+imagejpeg($im);
+
+imagedestroy($im);
 ?>
